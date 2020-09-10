2778b4b0b3390a124f757c9ed8a12925722d3f0f
pashakiz <pashakiz@gmail.com>
2020-09-10 20:10:30 +0300

    init files

---
+++ src/js/index.js
@@ -0,0 +1,77 @@
+import * as $ from 'jquery';
+import popper from 'popper.js';
+import bootstrap from 'bootstrap';
+import '@scss/main.scss'
+import 'owl.carousel'
+
+$(function() {
+
+    $(document).ready(function() {
+
+        //preloader
+        setTimeout(function() {
+            let preloader = $('#page-preloader');
+            if ( !preloader.hasClass('done') ) {
+                preloader.addClass('done');
+            }
+        }, 1000);
+
+        //mobile menu expand
+        $('.header').on('click', '.header__nav-toggle', function(){
+            let header_menu = $(this).closest('.header').find('.header-menu');
+            if ( $('.header').hasClass('expand') ) {
+                header_menu.slideUp(300);
+                setTimeout(function() {
+                    $('.header').removeClass('expand');
+                }, 300);
+            } else {
+                header_menu.slideDown(300);
+                $('.header').addClass('expand');
+            }
+        });
+
+        //upload photo
+        $('.custom-file-input').on('change', function(){
+            let imageUrl = 'img/photos/ava.jpg';
+            $('.profile-photo').css('background-image', 'url(' + imageUrl + ')');
+        });
+
+        //slider (Profile gallery) for mobile
+        $('.owl-carousel.user-profile-slider').owlCarousel({
+            items: 1,
+            dots:true
+        });
+
+        //slider (Profile gallery) for desktop
+        $('.owl-carousel.user-profile-gallery').owlCarousel({
+            loop: true,
+            dots: false,
+            items: 4,
+            autoWidth: true,
+            responsive:{
+                768:{
+                    margin: 5
+                },
+                992:{
+                    margin: 5
+                },
+                1200:{
+                    margin: 10
+                },
+                1410:{
+                    margin: 11
+                }
+            }
+        });
+
+        //gallery on desktop
+        $('.user-profile-gallery').on('click', '.user-profile-gallery__item', function(){
+            let url = $(this).css('background-image');
+            url = url.replace('url(','').replace(')','').replace(/\"/gi, "");
+            $('.user-list-item__img').css('background-image', 'url(' + url + ')');;
+        });
+
+    });
+
+
+});
